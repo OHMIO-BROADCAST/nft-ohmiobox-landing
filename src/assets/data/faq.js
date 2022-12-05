76f6286708c2520d7426a18a0392497a541fc5ca
Mauricio Henao <atomauro@cosmos-2.local>
2022-12-05 15:47:12 -0500

    .

--- src/assets/data/faq.js
+++ src/assets/data/faq.js
@@ -1,27 +1,27 @@
 const data = [
   {
     title: "What is OHMIO Box ?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    text: "An OHMIO Box is a non-fungible token or non-fungible token is an encrypted digital asset. It is a special type of cryptographic token that represents something unique.",
   },
   {
     title: "How we can buy and invest NFT ?",
-    text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
+    text: "You can get whitelisted and buy at the cheapest price, or wait for mint is live. When you receive your NFT and got revealed, you can sell it based on the rarity and the attributes.",
   },
   {
     title: "Why we should choose OHMIO Box ?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    text: "Because you can earn while contributing to the development of a dedicated piece of hardware that uses the Internet of Things (IoT) to help the radio industry and decentralize broadcast communications.",
   },
   {
     title: "Where we can buy and sell NFts ?",
-    text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
+    text: "We use OpenSea (opensea.com) as the platform for the launch of the collection, but you can sell it on other marketplaces compatible with the Ethereum Blockchain. Such as Rarible (Rarible.com) or through a custom purchase/sale agreement.",
   },
   {
     title: "How secure is this token ?",
-    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    text: "This contract is a contract that implements the latest standards for NFT, ensuring the necessary methods for each user to be and feel safe. There are no hidden methods or proxy contracts or any hidden minting function. Please check yourself and you will be able to confirm it. The wonderful thing about the Blockchain is that anyone can audit.",
   },
   {
     title: "What is your contract address ?",
-    text: "It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur cites of the word in classical literature.",
+    text: "OHMIO Box is currently only available on the main ethereum network, at the following address: https://etherscan.io/token/0x7492e30d60d96c58ed0f0dc2fe536098c620c4c9",
   },
 ];
 
