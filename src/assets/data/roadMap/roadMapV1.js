d2af4b2e0df8f7c5e9230328685b1e105455ffc4
Mauricio Henao <atomauro@cosmos-2.local>
2022-12-05 14:28:41 -0500

    .

--- src/assets/data/roadMap/roadMapV1.js
+++ src/assets/data/roadMap/roadMapV1.js
@@ -5,50 +5,50 @@ const data = [
     {
         icon: checkIcon,
         title: "Phase 01 ",
-        subtitle: "10% Soldout",
-        text: "Since the 3333s is reproduced below for those interested. We sold 333 NFTs."
-    }, 
-    {
-        icon: checkIcon,
-        title: "Phase 02",
         subtitle: "Whitelist",
-        text: "Pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth"
+        text: "In this period you will be able to register with your wallet to receive exclusive discount and being the first ones in getting an OHMIO Box."
     }, 
     {
         icon: checkIcon,
-        title: "Phase 03",
+        title: "Phase 02 ",
+        subtitle: "33% Soldout",
+        text: "Since the 3333s is reproduced below for those interested. We sold 1099 NFTs via whitelist, almost 33%."
+    }, 
+    {
+        icon: checkIcon,
+        title: "Phase 03 ",
         subtitle: "Minting LIVE",
-        text: "A complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects"
+        text: "For a total of 33 days, we will be available to minting. Remember, all the NFTs will be reveal 10 days after Minting is over, so we can leave the market grow."
     }, 
     {
         icon: spinnerIcon,
-        title: "Phase 04",
+        title: "Phase 04 ",
         subtitle: "End Minting LIVE",
-        text: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from expound the actual"
+        text: "After 33 days the minting period will end and reveal period will begin."
     }, 
     {
         icon: spinnerIcon,
-        title: "Phase 05",
-        subtitle: "DApp Launch", 
-        text: "On the other hand, we denounce with righteous indignation and dislike men from expound the actual reproduced below for those interested"
+        title: "Phase 05 ",
+        subtitle: "NFT Reveal & DApp Launch", 
+        text: "On the other hand, we wanto encourage the utility of NFTs so we develop a Descentralized App to use it, we will bring a lot of oportunities into our ecosystem and community. We plan to make it live when reveal NFTs is done."
     }, 
     {
         icon: spinnerIcon,
-        title: "Phase 06",
+        title: "Phase 06 ",
         subtitle: "2nd Generation of NFTs", 
-        text: "Reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced"
+        text: "Wallets that adquired the 1st Generation of this NFTs, the OHMIO Box, will receive a special aidrop from this newest generation of NFTs, a technological bills character based. "
     },
     {
         icon: spinnerIcon,
-        title: "Phase 07",
+        title: "Phase 07 ",
         subtitle: "Launch Token OHMIO", 
-        text: "Reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced"
+        text: "After we ensure the DAO Functions and descentralized utilities, OHMIO token ERC-20 will be minted, like the transaction core of the ecosystem."
     },
     {
         icon: spinnerIcon,
-        title: "Phase 08",
+        title: "Phase 08 ",
         subtitle: "Launch IoT Hardware", 
-        text: "Reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced"
+        text: "The main objective of the OHMIO Broadcast project is to bring blockchain technologies to life in the broadcasting industry. So we are gonna announce in detail the real piece of hardware that is OHMIO Box. The future is IoT and Blockchain NFTs together!"
     }, 
 ]
 
